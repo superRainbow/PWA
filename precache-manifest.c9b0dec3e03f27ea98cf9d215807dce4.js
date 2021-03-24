f92e2d1c6c14700ea5a34975b0c285bc0a9b8fe3
superRainbow <g901612002@hotmail.com>
2021-03-24 06:44:25 +0000

    Deploying to gh-pages from @ superRainbow/PWA@84b15ac4c74ca67d6e59c5d7726a2354b384155e 🚀

---
+++ precache-manifest.c9b0dec3e03f27ea98cf9d215807dce4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "998507672a26794a51dd511b41a2aa36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7f9a8ffd474041f2f9b",
+    "url": "/static/css/main.cbc57988.chunk.css"
+  },
+  {
+    "revision": "6240d49d154c80e0df67",
+    "url": "/static/js/2.fe335762.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.fe335762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7f9a8ffd474041f2f9b",
+    "url": "/static/js/main.7c465126.chunk.js"
+  },
+  {
+    "revision": "65fcdcaa19a0d064aa66",
+    "url": "/static/js/runtime-main.21c8a8c7.js"
+  },
+  {
+    "revision": "27bc25b74a48de0c1f9d9e20dd37e76c",
+    "url": "/static/media/rainbow.27bc25b7.png"
+  }
+]);
