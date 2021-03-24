ccb998720d7ae7581d5668a0abf350ae86e82c57
superRainbow <superRainbow@users.noreply.github.com>
2021-03-24 06:46:21 +0000

    deploy: e737ad50b29d9f072219be977f6836d72ee31c75

---
+++ precache-manifest.d1d07d78c19edb3a13f314415a4fccd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35edc3e261e6053cd3bda51bd4a6eec3",
+    "url": "/PWA/index.html"
+  },
+  {
+    "revision": "894d5d9635d9db650fab",
+    "url": "/PWA/static/css/main.cbc57988.chunk.css"
+  },
+  {
+    "revision": "fa3c67726be6ffab54d9",
+    "url": "/PWA/static/js/2.ca39a16a.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/PWA/static/js/2.ca39a16a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "894d5d9635d9db650fab",
+    "url": "/PWA/static/js/main.023d2274.chunk.js"
+  },
+  {
+    "revision": "3354c05c11da5e69b03b",
+    "url": "/PWA/static/js/runtime-main.8de97741.js"
+  },
+  {
+    "revision": "27bc25b74a48de0c1f9d9e20dd37e76c",
+    "url": "/PWA/static/media/rainbow.27bc25b7.png"
+  }
+]);
